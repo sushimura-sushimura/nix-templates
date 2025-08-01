9b434e1b4ac98f6700e8d18665692e2db2bbc6a8
sushimura <zhengcunliaotailang@gmail.com>
2025-08-02 00:51:48 +0900

    再度修正

--- flake.nix
+++ flake.nix
@@ -1,26 +1,19 @@
 {
   description = "A collection of Nix flake templates.";
 
-  # 依存する他の flake を指定
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  # flake が提供するアウトプットを定義
+  # outputs は関数として定義
   outputs = { self, nixpkgs, flake-utils }:
     let
-      # 各システムに対応した pkgs を取得するヘルパー関数を定義
-      # これは、各システムに依存するアウトプット (例: devShells) を記述する際に使います。
-      pkgsFor = system: import nixpkgs {
-        inherit system;
-        # 他のオーバーレイなどが必要な場合はここに追加
-      };
+      # devShells などを定義するためのヘルパー関数を定義
+      pkgsFor = system: nixpkgs.legacyPackages.${system};
     in
     {
-      # --- テンプレートの定義 ---
-      # テンプレートは、outputs のトップレベルに直接定義する必要があります。
-      # eachDefaultSystem の中には含めないでください。
+      # --- templates は outputs のトップレベルに配置 ---
       templates = {
         java-maven-app-template = {
           description = "A simple Java Maven application template.";
@@ -28,16 +21,13 @@
         };
       };
 
-      # --- 開発シェル (devShell) の定義 ---
-      # devShells など、システムに依存する設定は eachDefaultSystem を使って記述します。
-      # これは、テンプレートそのものではなく、このリポジトリ自体の開発環境を定義するものです。
+      # --- devShells は各システムに対応させて定義 ---
       devShells = flake-utils.lib.eachDefaultSystem (system:
         let
           pkgs = pkgsFor system;
         in
         {
           default = pkgs.mkShell {
-            name = "general-nix-dev-shell";
             packages = with pkgs; [
               jdk17
               maven
