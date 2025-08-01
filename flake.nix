e9dabb7dd1058c033928cbcc6e7f98604fb562f7
sushimura <zhengcunliaotailang@gmail.com>
2025-08-02 00:50:04 +0900

    再度修正

--- flake.nix
+++ flake.nix
@@ -1,65 +1,47 @@
 {
-  # この flake の説明
-  description = "A collection of Nix flake templates for various projects.";
+  description = "A collection of Nix flake templates.";
 
   # 依存する他の flake を指定
   inputs = {
-    # Nixpkgs を指定。nixpkgs-unstable ブランチを使う
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    
-    # flake-utils を使って、各システムに対応した設定を簡単に記述する
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   # flake が提供するアウトプットを定義
   outputs = { self, nixpkgs, flake-utils }:
     let
-      # devShells などのために、各システムに対応した pkgs を取得するヘルパー関数
-      pkgsFor = system: nixpkgs.legacyPackages.${system};
+      # 各システムに対応した pkgs を取得するヘルパー関数を定義
+      # これは、各システムに依存するアウトプット (例: devShells) を記述する際に使います。
+      pkgsFor = system: import nixpkgs {
+        inherit system;
+        # 他のオーバーレイなどが必要な場合はここに追加
+      };
     in
     {
-      # --- ここが最も重要な修正点です ---
-      # テンプレートは、outputs のトップレベルに直接定義します。
-      # 各システムに依存しないため、eachDefaultSystem の中には含めません。
+      # --- テンプレートの定義 ---
+      # テンプレートは、outputs のトップレベルに直接定義する必要があります。
+      # eachDefaultSystem の中には含めないでください。
       templates = {
-        # java-maven-app-template という名前でテンプレートを公開
         java-maven-app-template = {
           description = "A simple Java Maven application template.";
-          # このテンプレートの実体があるディレクトリを指定
-          # flake.nix と同じ階層にある java-maven-app-template ディレクトリを指す
           path = ./java-maven-app-template;
-        };
-
-        # 他のテンプレートも同様にここに追加します
-        nextjs-app-template = {
-          description = "A Next.js application template.";
-          path = ./nextjs-app-template;
         };
       };
 
-      # --- devShells は各システムに対応させて定義 ---
+      # --- 開発シェル (devShell) の定義 ---
       # devShells など、システムに依存する設定は eachDefaultSystem を使って記述します。
+      # これは、テンプレートそのものではなく、このリポジトリ自体の開発環境を定義するものです。
       devShells = flake-utils.lib.eachDefaultSystem (system:
         let
           pkgs = pkgsFor system;
         in
         {
           default = pkgs.mkShell {
-            # シェルに入ったときに必要なツールをここに記述
             name = "general-nix-dev-shell";
             packages = with pkgs; [
-              # Git や curl など、どのテンプレートでも共通して使うツール
-              git
-              curl
-              
-              # その他の一般的なツール
-              # ...
+              jdk17
+              maven
             ];
-            
-            # シェルが起動したときに実行されるスクリプト (任意)
-            shellHook = ''
-              echo "Welcome to the Nix development shell!"
-            '';
           };
         }
       );
